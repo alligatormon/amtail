4af383e5469c93b816e4de88a5ee3e4351293d84
amoshi <amoshi.mandrakeuser@gmail.com>
2026-04-15 20:08:54 +0300

    fixed compilation

--- amtail_pcre.h
+++ amtail_pcre.h
@@ -3,3 +3,4 @@
 #include "common/selector.h"
 regex_match* amtail_regex_compile(char *regexstring);
 void amtail_regex_free(regex_match *rematch);
+uint8_t amtail_regex_exec(regex_match *rematch, char *regex_match_string, uint64_t regex_match_size);
